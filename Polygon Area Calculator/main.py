b3fba87245bd81791abc987d979cdab4210ba698
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-10-01 18:32:25 +0800

    Update main.py

--- Polygon Area Calculator/main.py
+++ Polygon Area Calculator/main.py
@@ -1,7 +1,12 @@
+import math
+
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
 
     def set_width(self, width):
         self.width = width
@@ -13,10 +18,10 @@ class Rectangle:
         return self.width * self.height
 
     def get_perimeter(self):
-        return 2 * self.width + 2 * self.height
+        return 2 * (self.width + self.height)
 
     def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** .5
+        return math.sqrt(self.width**2 + self.height**2)
 
     def get_picture(self):
         if self.width > 50 or self.height > 50:
@@ -26,13 +31,12 @@ class Rectangle:
     def get_amount_inside(self, shape):
         return (self.width // shape.width) * (self.height // shape.height)
 
-    def __str__(self):
-        return f"Rectangle(width={self.width}, height={self.height})"
-
-
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
+
+    def __str__(self):
+        return f'Square(side={self.width})'
 
     def set_side(self, side):
         self.width = side
@@ -43,6 +47,3 @@ class Square(Rectangle):
 
     def set_height(self, height):
         self.set_side(height)
-
-    def __str__(self):
-        return f"Square(side={self.width})"
